db828105b623d287d4c4da2da439932d4117e6d5
jingcheng <jingcheng070@gmail.com>
2023-09-14 12:40:57 +0800

    spring actuator, application properties, configuring spring boot server

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,10 +1,26 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class FunRestController {
+
+    //inject prop for:
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+
+    //expose new endpoint
+    @GetMapping("/teaminfo")
+    public String getTeamInfo(){
+        return "Coach: " +coachName+", TeamName: "+teamName;
+    }
+
     // expose "/"  that return "Hello World"
     @GetMapping("/")
     public String sayHello(){
