36fe707dea81d08bdc60859485b273636e91c4dd
jingcheng <jingcheng070@gmail.com>
2023-09-14 13:36:34 +0800

    set injection

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -3,9 +3,7 @@ package com.luv2code.springboot.demo.mycoolapp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication(
-//		scanBasePackages = {"com.luv2code.util","com.luv2code.springboot.demo.mycoolapp"}
-//)
+
 @SpringBootApplication
 public class MycoolappApplication {
 
