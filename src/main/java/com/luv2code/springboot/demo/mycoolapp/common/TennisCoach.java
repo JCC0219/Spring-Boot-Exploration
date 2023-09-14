40a0c5a77b10658062ab6d3f4acf4a0f65493686
jingcheng <jingcheng070@gmail.com>
2023-09-14 14:20:42 +0800

    lazy annotation

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/common/TennisCoach.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/common/TennisCoach.java
@@ -4,8 +4,10 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary
 public class TennisCoach implements Coach{
+    public TennisCoach(){
+        System.out.println("In constructor " + getClass().getSimpleName());
+    }
     @Override
     public String getDailyWorkout() {
         return "Practice you backhand volley";
