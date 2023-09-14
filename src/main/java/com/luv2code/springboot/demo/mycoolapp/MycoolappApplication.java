57f6376aef69ab5f1028a3e557087701c9a97a61
jingcheng <jingcheng070@gmail.com>
2023-09-14 23:23:57 +0800

    create user with JPA DAO

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/MycoolappApplication.java
@@ -1,7 +1,12 @@
 package com.luv2code.springboot.demo.mycoolapp;
 
+import com.luv2code.springboot.demo.mycoolapp.dao.StudentDAO;
+import com.luv2code.springboot.demo.mycoolapp.dao.StudentDAOImpl;
+import com.luv2code.springboot.demo.mycoolapp.entity.Student;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 
 @SpringBootApplication
@@ -11,4 +16,23 @@ public class MycoolappApplication {
 		SpringApplication.run(MycoolappApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner commandLineRunner(StudentDAO studentDAO){
+		return runner ->{
+			createStudent(studentDAO);
+		};
+	}
+
+	private void createStudent(StudentDAO studentDAO) {
+		//create the student object
+		System.out.println("Creating new student object .....");
+		Student tempStudent = new Student("Paul","Doe","paul@test.com");
+
+		//save the student object
+		System.out.println("Saving the student ...");
+		studentDAO.save(tempStudent);
+
+		//display the id of the saved student
+		System.out.println("Saved student. Generated id:" + tempStudent.getId());
+	}
 }
--- src/main/java/com/luv2code/springboot/demo/mycoolapp/common/Coach.java
+++
@@ -1,5 +0,0 @@
-package com.luv2code.springboot.demo.mycoolapp.common;
-
-public interface Coach {
-    String getDailyWorkout();
-}
