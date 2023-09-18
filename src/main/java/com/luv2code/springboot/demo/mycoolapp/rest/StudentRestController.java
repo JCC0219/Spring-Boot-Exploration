5c3a7dfbc07e3c6d1a5b3f15bc81981d1dd6371d
jingcheng <jingcheng070@gmail.com>
2023-09-18 11:44:27 +0800

    refactor: with @PostConstruct

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/StudentRestController.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/StudentRestController.java
@@ -1,6 +1,7 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
 import com.luv2code.springboot.demo.mycoolapp.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,12 +12,19 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> theStudents = new ArrayList<>();
+    private List<Student> theStudents ;
+
+    //define @PostConstruct to load student data only once
+    @PostConstruct
+    public void loadData (){
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("Poornima","Patel"));
         theStudents.add(new Student("Mario","Rossi"));
         theStudents.add(new Student("Mary","Smith"));
+    }
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+
 
         return theStudents;
     }
