4ea7990ae45640483fa7f99f3bd9acdd4662cfe3
jingcheng <jingcheng070@gmail.com>
2023-09-18 11:40:44 +0800

    Spring Rest POJO

--- src/main/java/com/luv2code/springboot/demo/mycoolapp/entity/Student.java
+++ src/main/java/com/luv2code/springboot/demo/mycoolapp/entity/Student.java
@@ -1,39 +1,17 @@
 package com.luv2code.springboot.demo.mycoolapp.entity;
 
-import jakarta.persistence.*;
-
-@Entity
-@Table(name="student")
 public class Student {
-    //define fields
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "id")
-    private int id;
 
-    @Column(name = "first_name")
     private String firstName;
-    @Column(name = "last_name")
     private String lastName;
-    @Column(name = "email")
-    private String email;
 
-    //constructors
-    public Student(String firstName, String lastName, String email) {
+    public Student (){
+
+    }
+
+    public Student(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
-    }
-    public Student(){}
-
-    //define getters/setters
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
     }
 
     public String getFirstName() {
@@ -51,27 +29,4 @@ public class Student {
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-
-//define toString() method
-
-    @Override
-    public String toString() {
-        return "student{" +
-                "id=" + id +
-                ", firstName='" + firstName + '\'' +
-                ", lastName='" + lastName + '\'' +
-                ", email='" + email + '\'' +
-                '}';
-    }
 }
-
-
